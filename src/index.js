88f55f96cadc20bd1fde72acf1d180c707f14204
Souvik Mondal <warriorfish456@gmail.com>
2022-10-19 08:36:54 +0530

    Add Mongoose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import express from "express";
+import express from 'express'
+import mongoose from 'mongoose'
 
-const app = express();
+const app = express()
+mongoose
+    .connect('mongodb://localhost:27017/testwatertube')
+    .then(() => {
+        console.log('Connected to mongodb')
+    })
+    .catch((err) => {
+        throw err
+    })
 
-
-app.listen(8080,()=>{
-    console.log("Server started on port 8080");
+app.listen(8080, () => {
+    console.log('Server started on port 8080')
 })
