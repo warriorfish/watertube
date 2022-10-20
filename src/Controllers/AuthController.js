724d7e091cd95c5b6b0d7c9625d84ac10151bc25
Souvik Mondal <warriorfish456@gmail.com>
2022-10-20 19:35:04 +0530

    Move controller functions to proper file

---
+++ src/Controllers/AuthController.js
@@ -0,0 +1,22 @@
+import usersModel from '../Models/user.js'
+import bcrypt from 'bcrypt'
+import {createToken} from '../utils.js'
+
+const signup = async (req, res) => {
+    const { userName, email, password } = req.body
+    const encPassword = await bcrypt.hash(password, 10)
+    const user = new usersModel({
+        userName: userName,
+        email: email,
+        password: encPassword,
+    })
+    const userData = await user.save()
+    console.log(`Created user successfully with id: ${userData._id}`)
+
+    const webToken = createToken(userData._id);
+    res.status(200).json({
+        "jwtToken": webToken,
+    })
+};
+
+export {signup};
--- src/Routers/auth.js
+++ src/Routers/auth.js
@@ -1,25 +1,11 @@
 import express from 'express'
-import usersModel from '../Models/user.js'
-import bcrypt from 'bcrypt'
-import {createToken} from '../utils.js'
+import { signup } from '../Controllers/AuthController.js'
 
 const router = express.Router()
 
-router.post('/signup', async (req, res) => {
-    const { userName, email, password } = req.body
-    const encPassword = await bcrypt.hash(password, 10)
-    const user = new usersModel({
-        userName: userName,
-        email: email,
-        password: encPassword,
-    })
-    const userData = await user.save()
-    console.log(`Created user successfully with id: ${userData._id}`)
+router.post('/signup', signup)
 
-    const webToken = createToken(userData._id);
-    res.status(200).json({
-        "jwtToken": webToken,
-    })
-})
+
+
 
 export default router
