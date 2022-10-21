3fd0a40229796c8d02c5e79995840e75dbb6006d
Souvik Mondal <warriorfish456@gmail.com>
2022-10-21 06:11:44 +0530

    Add jwt token varifier

--- src/utils.js
+++ src/utils.js
@@ -10,4 +10,29 @@ function createToken(userId) {
     return token
 }
 
-export { createToken }
+function validateToken(req,res,next) {
+    const authToken = req.headers.authorization
+    if (!authToken) {
+        res.status(401).json({
+            errorMsg: "No authToken found",
+        })
+        return
+    }
+    
+    let userId
+
+    jwt.verify(authToken,process.env.SECRET_KEY,(err,decoded)=>{
+        if (err) {
+          res.status(401).json({
+            errorMsg: "Invalid authToken"
+          })
+          return  
+        }
+
+        userId = decoded.id
+        req.body.userId = userId
+        next()
+    })
+}
+
+export { createToken,validateToken }
